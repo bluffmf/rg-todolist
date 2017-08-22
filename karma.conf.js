7ef72711a3e41957fc18842efa066a0c0dde143b
Dmitry <bluffmf@gmail.com>
2017-08-22 19:33:43 +0300

    third one

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// concurrent \"npm install\" \" ng serve\" 
+
 module.exports = function (config) {
   config.set({
     basePath: '',
